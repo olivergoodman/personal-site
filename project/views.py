1ff72fa34adf87f1f2a50bdb28c4d57773073c5a
olivergoodman <ollygreatman@gmail.com>
2016-01-12 13:41:10 -0600

    commit

--- project/views.py
+++ project/views.py
@@ -2,24 +2,16 @@ from flask import Flask, render_template
 from project import app, db
 from project.models import Page
 
-
+#initializing 2 Page objects, index and contact
 index_page = Page("index")
 contact_page = Page("contact")
-
-def displayTable():
-  cursor = conn.execute("SELECT name, hits  from PAGEHITS")
-  for row in cursor:
-    print "NAME = ", row[0]
-    print "HITS = ", row[1], "\n"
   
 @app.route('/')
 @app.route('/index')
-
 def index():
   page_title = 'Oliver Goodman' 
   print 'index was called'
   index_page.visitPage()
-  displayTable()
   return render_template('index.html',
                           title = "Oliver Goodman | Home",
                           page_title = page_title)
@@ -28,7 +20,6 @@ def index():
 def contact(): 
   page_title = 'Contact'
   contact_page.visitPage()
-  displayTable()
   return render_template('contact.html',
 							           title = 'Oliver Goodman | Contact',
 							           page_title = page_title)
